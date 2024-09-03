479cc3a1cf782382810e3778efb18fca438c65f5
Akash Kumar <93832364+aakashhkumar@users.noreply.github.com>
2024-09-03 11:13:37 +0530

    Scalene Triangle.cpp

--- Codechef/Scalene Triangle.cpp
+++ Codechef/Scalene Triangle.cpp
@@ -5,7 +5,7 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-        int a,b,c;
+        int a,b,c; 
         cin>>a>>b>>c;
         // your code goes here
         if(a!=b && b!=c && c!=a ){
