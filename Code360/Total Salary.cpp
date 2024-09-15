34d258aff98583cb139b5d681ea3ad3371f56407
Akash Kumar <93832364+aakashhkumar@users.noreply.github.com>
2024-09-15 17:46:04 +0530

    Total Salary.cpp

--- Code360/Total Salary.cpp
+++ Code360/Total Salary.cpp
@@ -5,7 +5,7 @@ Write a program to calculate the total salary of a person. The user has to enter
 where : 
 HRA   = 20% of basic    
 DA    = 50% of basic 
-Allow = 1700 if grade = ‘A’
+Allow = 1700 if grade = ‘A’ 
 Allow = 1500 if grade = ‘B’    
 Allow = 1300 if grade = ‘C' or any other characters 
 PF    = 11% of basic. 
