98c2c8b5d92d3120877bf20a1e0cd0c3ea72a21a
Akash Kumar <93832364+aakashhkumar@users.noreply.github.com>
2024-12-28 11:06:17 +0530

    AB Difference to AB Difference.cpp

--- Codechef/AB Difference.cpp
+++ Codechef/AB Difference.cpp
@@ -2,7 +2,7 @@ AB Difference
 Chef is taking his baby steps into the world of programming.
 The very first program he's tasked to write is as follows:  
 "Given two integers A and B, print A + B."  
-Unfortunately, Chef makes a typo: his program outputs A × B instead of A + B. 
+Unfortunately, Chef makes a typo: his program outputs A × B instead of A + B.  
 Given the values of A and B, can you help Chef find the absolute difference between the correct answer and the value his program prints?
  
    
